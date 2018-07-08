a38b338d401634d2fab1d4cb63e0ee0bc79a05e5
antonio-sp <40887019+antonio-sp@users.noreply.github.com>
2018-07-09 01:29:07 +0200

    ee

---
+++ Server_AmministratoreInventario/Server_AmministratoreInventario/InventarioController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_AmministratoreInventario
+{
+    class InventarioController : IGestioneInventario
+    {
+        public InventarioController()
+        {
+           
+        }
+
+        public void aggiungiProdotto()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void eliminaProdotto()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void modificaProdotto()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
