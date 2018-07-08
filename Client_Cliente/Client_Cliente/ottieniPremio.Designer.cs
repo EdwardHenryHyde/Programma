6cc29f5492ccfba07ecfdafb373587358435aad0
Andrea <39404129+andrea-mengascini@users.noreply.github.com>
2018-07-08 19:58:41 +0200

    dd

--- Client_Cliente/Client_Cliente/ottieniPremio.Designer.cs
+++ Client_Cliente/Client_Cliente/ottieniPremio.Designer.cs
@@ -89,6 +89,7 @@
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Pentole";
             this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // radioButton2
             // 
@@ -100,6 +101,7 @@
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Servizio Piatti";
             this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // radioButton3
             // 
--- Client_Cliente/Client_Cliente/ottieniPremio.cs
+++ Client_Cliente/Client_Cliente/ottieniPremio.cs
@@ -12,9 +12,14 @@ namespace Client_Cliente
 {
     public partial class ottieniPremio : Form
     {
+        int saldo = 2300;
+
         public ottieniPremio()
         {
             InitializeComponent();
+            label6.Text = saldo.ToString();
+            label7.ResetText();
+            label9.Text = saldo.ToString();
         }
 
         private void closeButton_Click(object sender, EventArgs e)
@@ -27,6 +32,20 @@ namespace Client_Cliente
         private void elencoScontriniButton_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            //2300
+            label7.Text = "-900";
+            label9.Text = "1400";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            //2300
+            label7.Text = "-600";
+            label9.Text = "1400";
         }
     }
 }
